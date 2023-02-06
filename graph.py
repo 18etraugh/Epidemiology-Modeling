4e6bf772f8746ae5356276088439cb32f87e47ac
Ethan Traugh <Ethantraugh18@gmail.com>
2023-02-06 13:14:57 -0600

    fixed network sim. Needs to be commented and needs to create graphs

--- graph.py
+++ graph.py
@@ -1,13 +1,12 @@
 import networkx as nx
 import random
+import matplotlib.pyplot as plt
 
 t = 200
 count_history = {}
 node_status = [{}] * t
 
-## make node_status an aray of dictionaries
-## add in one step through t
-def count (d):
+def count (d, N):
     infected = 0
     susceptible = 0
     recovered = 0
@@ -18,32 +17,31 @@ def count (d):
             recovered += 1
         else:
             susceptible += 1
-    return ({'I' : infected,
-             'S' : susceptible,
-             'R' : recovered})
+    return ({'I' : infected/N,
+             'S' : susceptible/N,
+             'R' : recovered/N})
 
-def step (graph, b, y, i):
+def step (graph, N, b, y, i):
     nodes = list(graph.nodes())
     for node in nodes:
         if (node_status[i-1][node] == 'S'):
             neighbor = random.choice(nodes)
             if (node_status[i-1][neighbor] == "I" and (random.random() <= b)):
                 node_status[i][node] = 'I'
+                graph.nodes[node]['status'] = 'I'
         elif (node_status[i-1][node] == 'I' and random.random() <= y):
             node_status[i][node] = 'R'
-    count_history[i] = count(node_status[i])
+            graph.nodes[node]['status'] = 'R'
+    count_history[i] = count(node_status[i], N)
+    return graph
 
 
 
 
-def runOneSim (I, S, R, t, b, y):
+def runOneSim (graph, N, I, S, R, max_time, b, y):
     # Read the edge-list file and create an undirected graph
-    G = nx.read_edgelist('example.txt', delimiter='  ', create_using=nx.Graph())
+    G = nx.read_edgelist(graph, delimiter='  ', create_using=nx.Graph())
 
-    # Total number of nodes in the graph
-    N = G.number_of_nodes()
-
-    # Assign initial fractions for I, S, and R
 
     # Assign a status for each node based on the given fractions
     num_infected = int(N * I)
@@ -56,22 +54,27 @@ def runOneSim (I, S, R, t, b, y):
 
     for node in nodes[:num_infected]:
         node_status[0][node] = 'I'
+        G.nodes[node]['status'] = 'I'
     for node in nodes[num_infected:num_infected+num_susceptible]:
         node_status[0][node] = 'S'
+        G.nodes[node]['status'] = 'S'
     for node in nodes[num_infected+num_susceptible:]:
         node_status[0][node] = 'R'
+        G.nodes[node]['status'] = 'R'
 
     
-    count_history[0] = count(node_status[0])
+    count_history[0] = count(node_status[0], N)
 
-    for i in range(1, t):
-        step (G, b, y, i)
+    for i in range(1, max_time):
+        graph = step (G, N, b, y, i)
+        if i == 1 or i == 
     return (count_history.copy())
 
-def avg(sims, t):
+def avg(sims, t, N):
     avgI = []
     avgS = []
     avgR = []
+    numberOfSims = len (sims)
     for t in range (1, t):
         i = 0
         s = 0
@@ -81,22 +84,22 @@ def avg(sims, t):
             s += sim[t]["S"]
             r += sim[t]["R"]
         
-        avgI.append(i/t)
-        avgS.append(s/t)
-        avgR.append(r/t)
+        avgI.append(i/numberOfSims)
+        avgS.append(s/numberOfSims)
+        avgR.append(r/numberOfSims)
     return (avgI, avgS, avgR)
 
 
-def run (simAmount, i, s, r, t, b, y):
+def run (simAmount, graph, N, i, s, r, max_time, b, y):
     allSims = [{}] * simAmount
-    for i in range (0, simAmount):
-        allSims[i] = runOneSim (i, s, r, t, b, y)
+    for simNumber in range (0, simAmount):
+        allSims[simNumber] = runOneSim (graph, N, i, s, r, max_time, b, y)
     for sim in allSims:
         print (sim)
         print ()
         print ()
     
-    I, S, R = avg (allSims, t)
+    I, S, R = avg (allSims, max_time, N)
     print ("Average susceptible at each step: ")
     print (S)
     print ()
@@ -109,7 +112,7 @@ def run (simAmount, i, s, r, t, b, y):
     print (R)
     print ()
 
-run (3, .05, .95, 0, 10, .1, .05)
+run (1, 'smallerExample.txt', 100, .1, .9, 0, 200, .1, .05)
 
 
 
